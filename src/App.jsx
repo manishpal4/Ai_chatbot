2f8922ea9115efce31e4c143ced89bf57abf820d
manishpal <manishkumar9306074325@gmail.com>
2024-09-15 17:08:07 +0530

    Design added using tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,43 @@ import axios from "axios";
 
 function App() {
   const [question, setQuestion] = useState("");
-  const [awnser, setAwnser]=useState("");
+  const [awnser, setAwnser] = useState("");
 
   async function generateAwnser() {
     setAwnser("Loading...");
     const response = await axios({
-      url:`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${import.meta.env.VITE_API_KEY }`,
+      url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${
+        import.meta.env.VITE_API_KEY
+      }`,
       method: "post",
       data: {
         contents: [{ parts: [{ text: question }] }],
       },
     });
-    
-    setAwnser(response["data"]["candidates"][0]["content"]["parts"][0]["text"])
-    
+
+    setAwnser(response["data"]["candidates"][0]["content"]["parts"][0]["text"]);
   }
 
   return (
     <>
-      <div>AI Chat Bot</div>
-      <textarea rows={10} cols={30} placeholder="Type Your Question Here." value={question} onChange={(e)=> setQuestion(e.target.value)}></textarea>
-      <button onClick={generateAwnser}>Generate Awnser</button>
-      <pre>{awnser}</pre>
+      <div className=" text-5xl text-blue-700 font-sans font-bold mx-auto flex pt-7 justify-center items-center">
+        <span className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2">
+          AI
+        </span>{" "}
+        Chat Bot
+      </div>
+      <div className="justify-center items-center flex">
+        <textarea
+          className="p-3 w-3/4 my-3 mx-0 resize-none justify-center border-2 rounded-tl-lg rounded-bl-lg border-black"
+          rows={1}
+          cols={30}
+          placeholder="Type Your Question Here."
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        ></textarea>
+        <button className="bg-green-600 my-5 px-4 py-3.5 rounded-tr-lg rounded-br-lg text-white font-medium justify-center" onClick={generateAwnser}>Generate Awnser</button>
+      </div>
+      <textarea className="justify-center items-center flex read-only:  mx-auto p-3 border-none bg-gray-100 rounded-lg" rows={15} cols={160} name="" value={awnser} placeholder="Your Awnser will apear here." id="">{awnser}</textarea>
     </>
   );
 }
